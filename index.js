55523a5a0174b6bd8c8a783b2c34c8cba7d0f6d5
Adam Schwartz <adamberard24@gmail.com>
2021-03-20 19:14:51 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (endLocation){
+    let distance
+    if (endLocation >= 42){
+ (distance = endLocation - 42)}
+else { (distance = 42 - endLocation)}
+return distance
+}
+
+function distanceFromHqInFeet (endLocation) {
+   return (distanceFromHqInBlocks (endLocation) * 264)
+}
+
+function distanceTravelledInFeet(startLocation, endLocation) {
+let distance
+if (startLocation >= endLocation) {distance = (startLocation - endLocation)*264}
+if (startLocation < endLocation) {distance = (endLocation - startLocation)*264}
+return distance
+}
+
+function calculatesFarePrice (startLocation, endLocation) {
+    let total
+    if (distanceTravelledInFeet (startLocation, endLocation) <= 400) {total = 0}
+    if (distanceTravelledInFeet (startLocation, endLocation) > 400 && distanceTravelledInFeet (startLocation, endLocation) <= 2000) {total = (distanceTravelledInFeet (startLocation, endLocation) - 400) * .02}
+    if (distanceTravelledInFeet (startLocation, endLocation) > 2000 && distanceTravelledInFeet (startLocation, endLocation) <= 2500) {total = 25}
+    if (distanceTravelledInFeet (startLocation, endLocation) > 2500) {total = 'cannot travel that far'}
+    return total
+}
